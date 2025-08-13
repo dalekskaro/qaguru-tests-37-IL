35790269922ca4a73afc80672ddcc898d8aa47e1
Irina Attano <lobova394@gmail.com>
2025-08-13 08:58:25 +0300

    optimize import

--- src/test/java/lesson4/HomeworkSelenideWikiTest.java
+++ src/test/java/lesson4/HomeworkSelenideWikiTest.java
@@ -4,9 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.itemWithText;
 import static com.codeborne.selenide.CollectionCondition.texts;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;

e5047b8dd850a571b562c020df2325c71410bb50
Irina Attano <lobova394@gmail.com>
2025-08-13 09:08:33 +0300

    add empty lines

--- src/test/java/lesson4/HomeworkSelenideWikiTest.java
+++ src/test/java/lesson4/HomeworkSelenideWikiTest.java
@@ -21,11 +21,14 @@ public class HomeworkSelenideWikiTest {
     void selenideWikiTest() {
         //- Откройте страницу Selenide в Github
         open("/selenide/selenide");
+
         //- Перейдите в раздел Wiki проекта
         $("#wiki-tab").click();
+
         //- Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-pages-box").$(byText("Show 3 more pages…")).scrollTo().click();
         $$("#wiki-pages-box").shouldHave(texts("SoftAssertions"));
+
         //- Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
         $("#wiki-pages-box").$(byText("SoftAssertions")).click();
         $(byText("3. Using JUnit5 extend test class:")).shouldBe(visible);

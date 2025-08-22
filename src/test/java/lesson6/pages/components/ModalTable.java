0debb0c8bec47e0038f06f84b543d9ccfdaa9372
Irina Attano <lobova394@gmail.com>
2025-08-22 21:44:44 +0300

    add pageobjects for Student Registration Form demo-qa

---
+++ src/test/java/lesson6/pages/components/ModalTable.java
@@ -0,0 +1,25 @@
+package lesson6.pages.components;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ModalTable {
+
+    SelenideElement submittingModalLabel = $("#example-modal-sizes-title-lg"),
+            submittingModalTable = $(".table-responsive");
+
+    public void checkDataInSubmittingModalTable(String label, String value) {
+        submittingModalTable.$(byText(label)).parent().shouldHave(text(value));
+    }
+
+    public void checkResultInSubmittingModalLabel(String value) {
+        submittingModalLabel.shouldHave(text(value));
+    }
+
+    public void checkResultInSubmittingModalTableThead(String value) {
+        submittingModalTable.$("thead").shouldHave(text(value));
+    }
+}
---
+++ src/test/java/lesson6/tests/BaseTest.java
@@ -0,0 +1,24 @@
+package lesson6.tests;
+
+import com.codeborne.selenide.Configuration;
+import lesson6.pages.StudentRegistrationPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
+    StudentRegistrationPage studentRegistrationPage = new StudentRegistrationPage();
+
+    @BeforeAll
+    static void setUpBrowser() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @AfterEach
+    void closeBrowserDriver() {
+        closeWebDriver();
+    }
+}
